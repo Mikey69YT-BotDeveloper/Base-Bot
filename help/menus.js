e1bf7ace2da7473b9d5c4e2a349f519f7498de17
Mikey69YT <miky69yt@gmail.com>
2022-03-15 11:30:49 +0100

    Para vosotros jugadores

---
+++ help/menus.js
@@ -0,0 +1,296 @@
+const convertidoresmenu = (prefix) => {
+  return `*CONVERTIDORES*
+  🎨 ${prefix}sticker (Foto/video) 
+  🎨 ${prefix}sticker2 (Nombre|Autor)
+  🎨 ${prefix}take (Nombre|Autor)
+  🎨 ${prefix}exif (Nombre|Autor)
+  🎨 ${prefix}ttp
+  🎨 ${prefix}ttp2
+  🎨 ${prefix}attp
+  🎨 ${prefix}attp2
+  🎨 ${prefix}attp3
+  🎨 ${prefix}smeme (txt|text2)
+  🎨 ${prefix}smeme2
+  🎨 ${prefix}toimg
+  🎨 ${prefix}togif
+  🎨 ${prefix}tovid
+  🎨 ${prefix}toav
+  🎨 ${prefix}rvo
+  🎨 ${prefix}triggered
+  🎨 ${prefix}passed
+  🎨 ${prefix}wasted
+  🎨 ${prefix}wanted
+  🎨 ${prefix}gtav
+  🎨 ${prefix}imgrip
+  🎨 ${prefix}imgrip2
+  🎨 ${prefix}scelda
+  🎨 ${prefix}emoji_app
+  🎨 ${prefix}emoji_wa
+  🎨 ${prefix}emoji_fb
+  🎨 ${prefix}emoji_go
+  🎨 ${prefix}emoji_mo
+  🎨 ${prefix}emoji_tw
+  🎨 ${prefix}emoji_sa
+  🎨 ${prefix}emoji_op
+  🎨 ${prefix}emoji_pi
+  🎨 ${prefix}emoji_ht
+  🎨 ${prefix}emoji_lg`
+}
+
+const descargasmenu = (prefix) => {
+  return `*DESCARGAS*
+  📥 ${prefix}play
+  📥 ${prefix}play2
+  📥 ${prefix}playvid
+  📥 ${prefix}playvid2
+  📥 ${prefix}ytmp3
+  📥 ${prefix}ytmp4
+  📥 ${prefix}instagram [error]
+  📥 ${prefix}igstory [error]
+  📥 ${prefix}tiktok [error]
+  📥 ${prefix}tiktokaudio [error]
+  📥 ${prefix}mdfire`
+}
+
+const busquedamenu = (prefix) => {
+  return `*BUSQUEDA*
+  🔎 ${prefix}ytsearch
+  🔎 ${prefix}ytstalk
+  🔎 ${prefix}igstalk
+  🔎 ${prefix}playstore`
+}
+const educacionmenu = (prefix) => {
+  return `*EDUCACIÓN*
+  📚 ${prefix}covid
+  📚 ${prefix}hora
+  📚 ${prefix}trad
+  📚 ${prefix}cal
+  📚 ${prefix}google
+  📚 ${prefix}wiki
+  📚 ${prefix}encode
+  📚 ${prefix}decode`
+}
+const logosmenu = (prefix) => {
+  return `*LOGOS*
+  🧩 ${prefix}blackpink
+  🧩 ${prefix}glich
+  🧩 ${prefix}retroneon
+  🧩 ${prefix}luzneon
+  🧩 ${prefix}gaming
+  🧩 ${prefix}3d
+  🧩 ${prefix}phlogo
+  🧩 ${prefix}light
+  🧩 ${prefix}marvel
+  🧩 ${prefix}3dbox
+  🧩 ${prefix}flame
+  🧩 ${prefix}matrix
+  🧩 ${prefix}breakwall
+  🧩 ${prefix}bneon`
+}
+const mediamenu = (prefix) => {
+  return `*MEDIA*
+  🎥 ${prefix}screenshot
+  🎥 ${prefix}tourl
+  🎥 ${prefix}tourl2
+  🎥 ${prefix}urltoimg
+  🎥 ${prefix}toav
+  🎥 ${prefix}tomp3
+  🎥 ${prefix}asupan
+  🎥 ${prefix}wame
+  🎥 ${prefix}ocr
+  🎥 ${prefix}sinfondo
+  🎥 ${prefix}escribe
+  🎥 ${prefix}tinyurl (acortador)`
+}
+const funcionesmenu = (prefix) => {
+  return `*FUNCIÓNES*
+  🍒 ${prefix}random
+  🍒 ${prefix}leermas (texto1|texto2)
+  🍒 ${prefix}reenviado (texto)
+  🍒 ${prefix}fake (@tag|texto1|texto2)
+  🍒 ${prefix}tagme
+  🍒 ${prefix}mention (549xxx)
+  🍒 ${prefix}say`
+}
+
+const economiamenu = (prefix) => {
+  return `*ECONOMÍA*
+  🪙 ${prefix}shop
+  🪙 ${prefix}toplevel
+  🪙 ${prefix}cartera
+  🪙 ${prefix}getcartera (@tag)
+  🪙 ${prefix}buy (compra 💎)
+  🪙 ${prefix}buygcoin (compra 🎰)
+  🪙 ${prefix}transferd
+  🪙 ${prefix}adddiama
+  🪙 ${prefix}addgcoin
+  🪙 ${prefix}claim`
+}
+
+const diversionmenu = (prefix) => {
+  return `*DIVERSIÓN*
+  🎲 ${prefix}shipping 
+  🎲 ${prefix}pregunta
+  🎲 ${prefix}vor (verdad/reto)
+  🎲 ${prefix}verdad
+  🎲 ${prefix}reto
+  🎲 ${prefix}meme
+  🎲 ${prefix}ppt
+  🎲 ${prefix}topgay
+  🎲 ${prefix}ttc (@tag)
+  🎲 ${prefix}delttc
+  🎲 ${prefix}mates
+  🎲 ${prefix}simi
+  🎲 ${prefix}simi2
+  🎲 ${prefix}slot
+  🎲 ${prefix}dado
+  🎲 ${prefix}chiste`
+}
+
+const imagenesmenu = (prefix) => {
+  return `*IMAGENES*
+  🌅 ${prefix}imagen
+  🌅 ${prefix}par
+  🌅 ${prefix}pinterest
+  🌅 ${prefix}wallpaper
+  🌅 ${prefix}anime
+  🌅 ${prefix}loli
+  🌅 ${prefix}neko
+  🌅 ${prefix}waifu
+  🌅 ${prefix}girl
+  🌅 ${prefix}man
+  🌅 ${prefix}rip`
+}
+
+const nsfwmenu = (prefix) => {
+  return `*NSFW*
+  🔞 ${prefix}hentai
+  🔞 ${prefix}pack
+  🔞 ${prefix}nsfw_waifu
+  🔞 ${prefix}nsfw_loli
+  🔞 ${prefix}nsfw_neko`
+}
+
+const grupomenu = (prefix) => {
+  return `*GRUPOS*
+  👥 ${prefix}infogp
+  👥 ${prefix}infodetec
+  👥 ${prefix}group
+  👥 ${prefix}group2 [close/open]
+  👥 ${prefix}opentime
+  👥 ${prefix}closetime
+  👥 ${prefix}link
+  👥 ${prefix}mensajes
+  👥 ${prefix}online
+  👥 ${prefix}kick
+  👥 ${prefix}okick
+  👥 ${prefix}warn
+  👥 ${prefix}oadd
+  👥 ${prefix}staff
+  👥 ${prefix}dueñogp
+  👥 ${prefix}setdesc
+  👥 ${prefix}setname
+  👥 ${prefix}anularlink
+  👥 ${prefix}getbio (@tag)
+  👥 ${prefix}getperfil (@tag)
+  👥 ${prefix}perfil
+  👥 ${prefix}nivel
+  👥 ${prefix}rangos
+  👥 ${prefix}leido (Mensaje bot) 
+  👥 ${prefix}leveling [on/off]
+  👥 ${prefix}welcome [on/off]
+  👥 ${prefix}goodbye [on/off]
+  👥 ${prefix}simih [on/off]
+  👥 ${prefix}anticadena [on/off]
+  👥 ${prefix}antiviewonce [on/off]
+  👥 ${prefix}antilink [on/off]
+  👥 ${prefix}interaction [on/off]
+  👥 ${prefix}antivirtual [on/off]
+  👥 ${prefix}nsfw [on/off]
+  👥 ${prefix}delete (mensaje bot)
+  👥 ${prefix}salir (dejo el grupo)`
+}
+
+const mencionesmenu = (prefix) => {
+  return `*MENCIONES*
+  🪀 ${prefix}tagall
+  🪀 ${prefix}tag
+  🪀 ${prefix}notify (texto)
+  🪀 ${prefix}hidetag
+  🪀 ${prefix}contag (Nombre|Num)
+  🪀 ${prefix}sticktag
+  🪀 ${prefix}imgtag`
+}
+
+const sesionmenu = (prefix) => {
+  return `*SESIÓN*
+  🤖 ${prefix}serbot
+  🤖 ${prefix}stopbot
+  🤖 ${prefix}listbots`
+}
+
+const avanzadomenu = (prefix) => {
+  return `*AVANZADO*
+  ⚙️ ${prefix}blocklist
+  ⚙️ ${prefix}banlist
+  ⚙️ ${prefix}listbanchat
+  ⚙️ ${prefix}liststickcmd
+  ⚙️ ${prefix}getstickcmd
+  ⚙️ ${prefix}premiumlist
+  ⚙️ ${prefix}checkpremium`
+}
+
+const ownermenu = (prefix) => {
+  return `*OWNER*
+  🐱 ${prefix}actualizar
+  🐱 ${prefix}modo (publico/privado)
+  🐱 ${prefix}stickcmd
+  🐱 ${prefix}delstickcmd
+  🐱 ${prefix}addprem
+  🐱 ${prefix}delprem
+  🐱 ${prefix}setprefix
+  🐱 ${prefix}ban
+  🐱 ${prefix}unban
+  🐱 ${prefix}clearchat
+  🐱 ${prefix}buscarmsg
+  🐱 ${prefix}listagp
+  🐱 ${prefix}antillamada
+  🐱 ${prefix}antidelete
+  🐱 ${prefix}autoread
+  🐱 ${prefix}informes
+  🐱 ${prefix}solicitudes
+  🐱 ${prefix}clonar
+  🐱 ${prefix}banchat
+  🐱 ${prefix}unbanchat
+  🐱 ${prefix}chatmode
+  🐱 ${prefix}buggc
+  🐱 ${prefix}simularw`
+}
+
+const infomenu = (prefix) => {
+  return `*INFORMACIÓN*
+  ℹ️ ${prefix}reglas
+  ℹ️ ${prefix}creador
+  ℹ️ ${prefix}info
+  ℹ️ ${prefix}gpbot
+  ℹ️ ${prefix}script
+  ℹ️ ${prefix}report
+  ℹ️ ${prefix}ping
+  ℹ️ ${prefix}runtime
+  ℹ️ ${prefix}estado
+  ℹ️ ${prefix}join
+  ℹ️ ${prefix}ayuda`
+}
+
+
+module.exports =  { convertidoresmenu, descargasmenu, busquedamenu, educacionmenu, logosmenu, mediamenu, funcionesmenu, economiamenu, diversionmenu, imagenesmenu, nsfwmenu, grupomenu, mencionesmenu, sesionmenu, avanzadomenu, ownermenu, infomenu } 
+
+    
+
+    
+
+    
+
+    
+
+    
